ad81077d4659855d9003a8cfcab376e72aaff15f
jbratcher <jeremybratcher@gmail.com>
2019-03-23 15:47:14 -0400

    feat: basic template rewrite, refactor: real gh pages homepage link and comment for template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <section className="App container-fluid text-center">
+
+        <header>
+          <h1>Header</h1>
         </header>
-      </div>
+
+        <main>
+          <p>
+            Main
+          </p>
+        </main>
+
+        <footer>
+          <h6>
+            Footer
+          </h6>
+        </footer>
+
+      </section>
     );
   }
 }
