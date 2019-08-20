7573720f72ed36ed6d4240c2d1a4e395d6fd92cc
jbratcher <jeremybratcher@gmail.com>
2019-08-19 20:07:00 -0400

    Updates

---
+++ precache-manifest.a020d067116840ce669a40dc8f1a1f09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbb45a9545bd7f7ac5c87976903f286",
+    "url": "/react-starter-kit/index.html"
+  },
+  {
+    "revision": "1b6a3e0b18f73e078970",
+    "url": "/react-starter-kit/static/css/main.eefd9873.chunk.css"
+  },
+  {
+    "revision": "c05d271bcde2df01cf12",
+    "url": "/react-starter-kit/static/js/2.3496c98e.chunk.js"
+  },
+  {
+    "revision": "1b6a3e0b18f73e078970",
+    "url": "/react-starter-kit/static/js/main.3cd93673.chunk.js"
+  },
+  {
+    "revision": "8bd1aa1f739450cb47b0",
+    "url": "/react-starter-kit/static/js/runtime~main.362905fa.js"
+  }
+]);
